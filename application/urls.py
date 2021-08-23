afb195853753e16903e8b82edc010853d7fe5bb5
atushi421 <a.yano.578@ms.saitama-u.ac.jp>
2021-08-24 05:02:48 +0900

    test google scholar

--- application/urls.py
+++ application/urls.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from django.urls import path, include  # include は委譲
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from .views import signup, activate
+from .views import signup, activate, index
 
 
-# トップページはこのように書く
-index_view = TemplateView.as_view(template_name='registration/index.html')
 
 urlpatterns = [
-    path('', login_required(index_view), name='index'),  # トップページをログイン必須とする
+    path('', login_required(index.IndexView.as_view()), name='index'),  # トップページをログイン必須とする
     path('', include('django.contrib.auth.urls')),  # ログイン関係で、django がもともと用意している URL とマッチした場合表示。login, logout などのURLが含まれている
     path('signup/', signup.SignUpView.as_view(), name='signup'),
     path('activate/<uidb64>/<token>/', activate.ActivateView.as_view(), name='activate'),  # <>はビュー側でパラメータとして受け取れる
--- application/views/index.py
+++ application/views/index.py
@@ -1,4 +1,42 @@
-from django.views.generic import TemplateView
+from django.views import View
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        keyword = "machine learning"
+        number = 10
+        search_results_df = get_search_results_df(keyword,number)
+        
+        context = {
+            'search_results' : search_results_df.to_html(),
+        }
+        
+        return render(request, 'registration/index.html', context)
+
+
+def get_search_results_df(keyword,number):
+    columns = ["rank", "title", "writer", "year", "citations", "url"]
+    df = pd.DataFrame(columns=columns) #表の作成
+    html_doc = requests.get("https://scholar.google.co.jp/scholar?hl=ja&as_sdt=0%2C5&num=" + str(number) + "&q=" + keyword).text
+    soup = BeautifulSoup(html_doc, "html.parser") # BeautifulSoupの初期化
+    tags1 = soup.find_all("h3", {"class": "gs_rt"})  # title&url
+    tags2 = soup.find_all("div", {"class": "gs_a"})  # writer&year
+    tags3 = soup.find_all(text=re.compile("引用元"))  # citation
+
+    rank = 1
+    for tag1, tag2, tag3 in zip(tags1, tags2, tags3):
+        title = tag1.text.replace("[HTML]","")
+        url = tag1.select("a")[0].get("href")
+        writer = tag2.text
+        writer = re.sub(r'\d', '', writer)
+        year = tag2.text
+        year = re.sub(r'\D', '', year)
+        citations = tag3.replace("引用元","")
+        se = pd.Series([rank, title, writer, year, citations, url], columns)
+        df = df.append(se, columns)
+        rank += 1
+    return df
